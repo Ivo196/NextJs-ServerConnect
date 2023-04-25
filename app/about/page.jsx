bb474f912526304dee05692692851a85b869d8f8
Ivo196 <ivo.tonioni@gmail.com>
2023-04-25 17:08:46 +0200

    first commit

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+function Page() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem doloremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem do loremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem doloremque quisquam? Quisquam, quidem dolorem</p>
+    </div>
+  );
+}
+
+export default Page;
+
---
+++ app/services/page.jsx
@@ -0,0 +1,10 @@
+function ServicePage() {
+    return (
+      <div>
+          <h1>Service Page</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, quisquam!</p>
+      </div>
+    )
+  }
+  
+  export default ServicePage
